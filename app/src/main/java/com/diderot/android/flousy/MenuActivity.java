94456c39d504a8623e5339ceab56da09b298ab26
sasd13 <samir_saidali@yahoo.fr>
2015-02-17 00:15:22 +0100

    add uncommitted files

---
+++ app/src/main/java/com/diderot/android/flousy/MenuActivity.java
@@ -0,0 +1,56 @@
+package com.diderot.android.flousy;
+
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+
+public class MenuActivity extends MyActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Disable previous
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        //Set activity color
+        this.activity_color = R.color.customGreen;
+        this.activity_topLine.setBackgroundResource(this.activity_color);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_myactivity, menu);
+
+        MenuItem item = menu.add(R.string.action_search);
+        item.setIcon(R.drawable.ic_action_search);
+        /*
+        if (Build.VERSION.SDK_INT >= 11) {
+            SearchView searchView = new SearchView(this);
+            item.setActionView(new SearchView(this));
+        }
+        */
+        super.onCreateOptionsMenu(menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
