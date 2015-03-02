35cc7e494e8ac41647b1a91282873b16372a29d9
sasd13 <samir_saidali@yahoo.fr>
2015-03-02 10:44:37 +0100

    Mise à jour des maquettes et du Manuel d'utilisateur

---
+++ app/src/main/java/com/diderot/android/flousy/SignInActivity.java
@@ -0,0 +1,40 @@
+package com.diderot.android.flousy;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SignInActivity extends MyActivity {
+
+    public static final int ACTIVITY_COLOR = R.color.customRed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Set activity color before everything
+        setActivityColor(ACTIVITY_COLOR);
+
+        //Set ActivityBar
+        setActivityBar(R.layout.layout_activitybarwithtitle);
+        setActivityBarTitle("Dépenses");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
