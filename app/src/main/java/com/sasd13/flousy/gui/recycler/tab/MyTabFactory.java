81fe5df2bc35b7228102184dd63eb4d009947aee
Samir <samir_saidali@yahoo.fr>
2016-07-10 16:36:29 +0200

    Less correction

--- app/src/main/java/com/sasd13/flousy/gui/recycler/tab/MyTabFactory.java
+++ app/src/main/java/com/sasd13/flousy/gui/recycler/tab/MyTabFactory.java
@@ -1,14 +1,14 @@
 package com.sasd13.flousy.gui.recycler.tab;
 
 import com.sasd13.androidex.gui.widget.IType;
+import com.sasd13.androidex.gui.widget.recycler.RecyclerFactory;
 import com.sasd13.androidex.gui.widget.recycler.RecyclerItem;
-import com.sasd13.androidex.gui.widget.recycler.tab.TabFactory;
 import com.sasd13.flousy.gui.recycler.MyRecyclerFactoryType;
 
 /**
  * Created by ssaidali2 on 02/07/2016.
  */
-public class MyTabFactory extends TabFactory {
+public class MyTabFactory extends RecyclerFactory {
 
     @Override
     public RecyclerItem makeItem(IType type) {
